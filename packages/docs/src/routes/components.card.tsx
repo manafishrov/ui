7aa9e7d37a3dc2756577d5009c0f6b07acd4186d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-02-25 20:47:13 +0100

    WIP: fix card

--- packages/docs/src/routes/components.card.tsx
+++ packages/docs/src/routes/components.card.tsx
@@ -10,7 +10,7 @@ import {
   CardContent,
   CardFooter,
 } from '@manafishrov/ui/card';
-import { H1, H2, Lead } from '@manafishrov/ui/typography';
+import { H1, Lead } from '@manafishrov/ui/typography';
 import { createFileRoute } from '@tanstack/solid-router';
 
 import * as m from '@/paraglide/messages';
@@ -27,21 +27,20 @@ const CardDocPage: Component = () => (
         <CardHeader>
           <CardTitle>Create project</CardTitle>
           <CardDescription>Deploy your new project in one-click.</CardDescription>
+          <CardAction>
+            <Button variant='link'>Share</Button>
+          </CardAction>
         </CardHeader>
         <CardContent>
           <div class='h-20 flex items-center justify-center rounded-md border border-dashed text-muted-foreground'>
             Form goes here
           </div>
         </CardContent>
-        <CardFooter class='flex justify-between'>
-          <CardAction
-            asChild={(props) => (
-              <Button variant='outline' {...props()}>
-                Cancel
-              </Button>
-            )}
-          />
-          <CardAction asChild={(props) => <Button {...props()}>Deploy</Button>} />
+        <CardFooter class='gap-2 flex-col'>
+          <Button class='w-full'>Deploy</Button>
+          <Button variant='outline' class='w-full'>
+            Cancel
+          </Button>
         </CardFooter>
       </Card>
     </div>
