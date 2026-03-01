2451ee1fbc66bbfcf40a396f00fbadc6de21dbb4
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-03-01 11:17:34 +0100

    WIP: fix link component

--- packages/lib/src/components/Link.tsx
+++ packages/lib/src/components/Link.tsx
@@ -7,11 +7,10 @@ export type LinkProps = RouterLinkProps & ButtonVariantProps & { class?: string 
 
 export const Link: Component<LinkProps> = (props) => {
   const [local, others] = splitProps(props, ['class', 'size', 'variant']);
+  const hasVariantOrSize = typeof local.variant === 'string' || typeof local.size === 'string';
+  const linkClass = hasVariantOrSize
+    ? buttonVariants({ class: local.class, size: local.size, variant: local.variant })
+    : local.class;
 
-  return (
-    <RouterLink
-      class={buttonVariants({ class: local.class, size: local.size, variant: local.variant })}
-      {...others}
-    />
-  );
+  return <RouterLink class={linkClass} {...others} />;
 };
