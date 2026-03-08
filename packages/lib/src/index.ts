566db56460d92b15cb573d931574449969ad3212
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2026-03-08 18:36:54 +0100

    fix: update export

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -16,6 +16,6 @@ export {
 } from '@ark-ui/solid/collection';
 export { tv, cx, cn, cnMerge, type VariantProps } from 'tailwind-variants';
 export { LocaleProvider, type LocaleProviderProps } from '@/Locale';
-export { ThemeProvider, useTheme, type Theme, type ThemeProviderProps } from '@/Theme';
+export { ThemeProvider, useTheme, Theme, type ThemeProviderProps } from '@/Theme';
 
 export { createMediaQuery } from '@/primitives/createMediaQuery';
